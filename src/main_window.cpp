7d5aa58c65939041181253115ebe48e45bf65b6a
Bloom Perez <valenplayer3000+code@gmail.com>
2026-09-12 21:24:30 -0400

    feat: add checks to the account
    It checks when there's no account it should show the wizard first.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,6 +1,8 @@
 #include "main_window.hpp"
-#include "account_wizard.hpp"
 #include "./ui_main_window.h"
+
+#include "src/account_wizard.hpp"
+#include "src/account_manager.hpp"
 
 #include <AeroQt/usericon.h>
 #include <QAction>
@@ -10,6 +12,21 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    auto *accounts = new AccountManager(this);
+    auto *api = new ApiClient(AccountWizard::officialInstanceUrl());
+
+    connect(accounts, &AccountManager::accountLoaded, this, [this, api](const Account &a) {
+        if (a.isValid())
+            api->setToken(a.token);
+        else {
+            auto *wizard = new AccountWizard(this);
+            connect(wizard, &QDialog::accepted, this, [accounts, wizard] {
+               // TODO: actually store straight to keychain
+            });
+            wizard->open();
+        }
+    });
+
     ui->setupUi(this);
 
     auto *userIcon = new Aero::UserIcon(Aero::UserIcon::Size_48, ui->centralwidget);
